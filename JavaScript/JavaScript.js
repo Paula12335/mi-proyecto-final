754e203d9af78afb2dbed524233f51b6f489df69
Paula12335 <alberonipauli@gmail.com>
2022-12-12 10:32:43 -0300

    modifica java css index rutas exoticos y agrego un video con fotos de los lugares en pagina rutas.

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -14,7 +14,7 @@ Edad = prompt('Ingrese su Edad')
 if(Edad >= 18){
   alert('Puedes entrar, eres mayor de edad.')
 }else{
-  alert('Eres menor, no puedes ingresar')
+  alert('Eres menor, entras bajo tu responsabilidad')
 }
 */
 
--- JavaScript/inicio.js
+++ JavaScript/inicio.js
@@ -3,5 +3,5 @@ Edad = prompt('Ingrese su Edad')
 if(Edad >= 18){
   alert('Puedes entrar, eres mayor de edad.')
 }else{
-  alert('Eres menor, no puedes ingresar')
+  alert('Eres menor, entras bajo tu propia responsabilidad.')
 }
